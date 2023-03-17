eb2d6b6186579a33d34b62af00072a41c8f8b794
louis <louis.texpro@gmail.com>
2023-03-17 23:46:37 +0100

    la fonction marcheeeeeee

---
+++ Anniversaire/web/TODO.php
@@ -0,0 +1,3 @@
+<?php
+//TODO vue generale qui est apellé avec un contenue, passage en appele twig avec des lien simplifié ( / => ecrire un msg , /lire => lire un message, /modif =>modifié un message)
+//TODO presentation des message, plusieur possibilité ?
